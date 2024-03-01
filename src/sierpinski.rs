f8371a0cb89bad728114f4e15cc122ccb2019085
Koji Saruya <koji.saruya@woven-planet.global>
2024-03-01 21:14:30 +0900

    Modulize sierpinski

---
+++ src/sierpinski.rs
@@ -0,0 +1,68 @@
+use rand::{thread_rng, Rng};
+
+pub fn sierpinski(
+    context: &web_sys::CanvasRenderingContext2d,
+    points: [(f64, f64); 3],
+    color: (u8, u8, u8),
+    depth: i8,
+) {
+    let [top, left, right] = points;
+    let left_middle = mid_point(top, left);
+    let right_middle = mid_point(top, right);
+    let bottom_middle = mid_point(left, right);
+    draw_triangle(&context, points, color);
+
+    let depth = depth - 1;
+
+    let mut rng = thread_rng();
+
+    let next_color = (
+        rng.gen_range(0..255),
+        rng.gen_range(0..255),
+        rng.gen_range(0..255),
+    );
+
+    if depth > 0 {
+        sierpinski(
+            &context,
+            [top, left_middle, right_middle],
+            next_color,
+            depth,
+        );
+        sierpinski(
+            &context,
+            [left_middle, left, bottom_middle],
+            next_color,
+            depth,
+        );
+        sierpinski(
+            &context,
+            [right_middle, bottom_middle, right],
+            next_color,
+            depth,
+        );
+    }
+}
+
+fn draw_triangle(
+    context: &web_sys::CanvasRenderingContext2d,
+    points: [(f64, f64); 3],
+    color: (u8, u8, u8),
+) {
+    let color_str = format!("rgb({}, {}, {})", color.0, color.1, color.2);
+    context.set_fill_style(&wasm_bindgen::JsValue::from_str(&color_str));
+
+    let [top, left, right] = points;
+    context.move_to(top.0, top.1);
+    context.begin_path();
+    context.line_to(left.0, left.1);
+    context.line_to(right.0, right.1);
+    context.line_to(top.0, top.1);
+    context.close_path();
+    context.stroke();
+    context.fill();
+}
+
+fn mid_point(point_1: (f64, f64), point_2: (f64, f64)) -> (f64, f64) {
+    ((point_1.0 + point_2.0) / 2.0, (point_1.1 + point_2.1) / 2.0)
+}
