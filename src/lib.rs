cb23001d5d9809a2332ec7dec8542941d1e7d748
Koji Saruya <koji.saruya@woven-planet.global>
2024-03-01 22:39:41 +0900

    Just add image..

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ mod sierpinski;
 
 use sierpinski::sierpinski;
 use wasm_bindgen::prelude::*;
-use web_sys::{console, window};
+use web_sys::{console, window, CanvasRenderingContext2d};
 
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
@@ -17,6 +17,32 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 pub fn main_js() -> Result<(), JsValue> {
     console_error_panic_hook::set_once();
 
+    let context = create_context();
+
+    wasm_bindgen_futures::spawn_local(async move {
+        let (success_tx, success_rx) = futures::channel::oneshot::channel::<()>();
+        let image = web_sys::HtmlImageElement::new().unwrap();
+        let callback = Closure::once(move || {
+            success_tx.send(());
+        });
+        image.set_onload(Some(callback.as_ref().unchecked_ref()));
+        image.set_src("Idle (1).png");
+
+        success_rx.await;
+        context.draw_image_with_html_image_element(&image, 0.0, 0.0);
+
+        sierpinski(
+            &context,
+            [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)],
+            (0, 255, 0),
+            10,
+        );
+    });
+
+    Ok(())
+}
+
+fn create_context() -> CanvasRenderingContext2d {
     let window = web_sys::window().unwrap();
     let document = window.document().unwrap();
     let canvas = document
@@ -31,12 +57,5 @@ pub fn main_js() -> Result<(), JsValue> {
         .unwrap()
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
         .unwrap();
-
-    sierpinski(
-        &context,
-        [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)],
-        (0, 255, 0),
-        10,
-    );
-    Ok(())
+    context
 }
